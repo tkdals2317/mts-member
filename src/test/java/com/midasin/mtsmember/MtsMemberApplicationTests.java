bbdab9a2f41d68957cd7a16235818528b7b436e7
lsm0506 <lsm0506@midasin.com>
2022-12-11 21:21:22 +0900

    Initial commit

---
+++ src/test/java/com/midasin/mtsmember/MtsMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midasin.mtsmember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtsMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
